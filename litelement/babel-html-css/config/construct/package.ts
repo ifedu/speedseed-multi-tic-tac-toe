ceb222c9bbf8b50dfc375efbb1184cc8e87539b4
Edu Pérez-Bermejo <ifedu@outlook.com>
2019-02-07 16:26:46 +0100

    no message

--- litelement/babel-html-css/config/construct/package.ts
+++ litelement/babel-html-css/config/construct/package.ts
@@ -1,7 +1,31 @@
 export default {
     "dependencies": {
-        "lit-element": "2.0.0-rc.5",
+        "lit-element": "2.0.1",
     },
-    "devDependencies": {},
+    "devDependencies": {
+        "@babel/core": "7.2.2",
+        "@babel/plugin-proposal-decorators": "7.3.0",
+        "@types/browser-sync": "0.0.42",
+        "@types/lodash": "4.14.120",
+        "@types/node": "10.12.21",
+        "@types/webpack": "4.4.24",
+        "babel-loader": "8.0.5",
+        "browser-sync": "2.26.3",
+        "connect-history-api-fallback": "1.6.0",
+        "css-loader": "2.1.0",
+        "gulp-htmlmin": "5.0.1",
+        "gulp-minify-inline": "1.1.0",
+        "gulp-plumber": "1.2.1",
+        "lit-element": "2.0.1",
+        "lodash": "4.17.11",
+        "raw-loader": "1.0.0",
+        "style-loader": "0.23.1",
+        "ts-loader": "5.3.3",
+        "ts-node": "8.0.2",
+        "tsconfig-paths": "3.8.0",
+        "tslint": "5.12.1",
+        "typescript": "3.3.1",
+        "webpack": "4.29.2"
+    },
     "scripts": {},
 }
